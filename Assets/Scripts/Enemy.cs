136e0a1b4c85890d203b6d128880e6d53bb9a239
XINLU CHEN <xinluchen.work@gmail.com>
2023-11-28 20:58:10 -0800

    movement and rotation
    Add player movement, and rotation based on the mouse position. Enemy tracking player and move to player

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -3,24 +3,40 @@ using UnityEngine;
 public class Enemy: PlayableObject
 {
     private string enemyName;
-    private float speed;
+    [SerializeField] private float speed;
     private EnemyType enemyType;
 
-    private void Start()
+    protected Transform target;
+
+    protected virtual void Start()
     {
-        Debug.Log(Utilities.DEVICE_ID);
-        Utilities.ShowDeviceID("RICO");
-        Debug.Log(Utilities.DEVICE_ID);
+        target = GameObject.FindWithTag("Player").transform;
+        Debug.Log("target: " + target.position);
+        //Debug.Log(Utilities.DEVICE_ID);
+        //Utilities.ShowDeviceID("RICO");
+        //Debug.Log(Utilities.DEVICE_ID);
         //Move(transform);
         //Shoot(Vector3.zero, 2.0f);
         //Die();
         //Attack(2.0f);
     }
-    
 
-    public override void Shoot(Vector3 direction, float speed)
+    protected virtual void Update()
     {
-        Debug.Log($"Shooting a bullet towards {direction} with a speed of {speed}");
+        if (target != null)
+        {
+            Move(target.position);
+        }
+        else
+        {
+            Move(speed);
+        }
+    }
+
+    public override void Shoot()
+    {
+        Debug.Log($"Shooting a bullet towards");
+        //Debug.Log($"Shooting a bullet towards {direction} with a speed of {speed}");
     }
 
     public override void Die()
@@ -41,6 +57,22 @@ public class Enemy: PlayableObject
 
     public override void Move(Vector2 direction, Vector2 target)
     {
+    }
+
+    public override void Move(float speed) // only move to right?
+    {
+        transform.Translate(Vector2.right * speed * Time.deltaTime);
+    }
+
+    public override void Move(Vector2 direction)
+    {
+        direction.x -= transform.position.x;
+        direction.y -= transform.position.y;
+
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0, 0, angle);
+
+        transform.Translate(Vector2.right * speed * Time.deltaTime);
     }
 
     public override void GetDamage(float damage)
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,9 +8,10 @@ public class Player: PlayableObject
 
     private Rigidbody2D playerRB;
 
-    public override void Shoot(Vector3 direction, float speed)
+    public override void Shoot()//Vector3 direction, float speed
     {
-        Debug.Log($"Shooting a bullet towards {direction} with a speed of {speed}");
+        Debug.Log("Player is shooting a bullet");
+        //Debug.Log($"Shooting a bullet towards {direction} with a speed of {speed}");
     }
 
     public override void Die()
@@ -18,11 +19,26 @@ public class Player: PlayableObject
         Debug.Log("Player Died");
     }
 
+    private void Start()
+    {
+        health = new Health(100f, 100f, 0.5f);
+        playerRB = GetComponent<Rigidbody2D>();
+
+    }
     public override void Move(Vector2 direction, Vector2 target)
     {
+        //Debug.Log("player direction: " + direction);
+
         playerRB.velocity = direction * speed * Time.deltaTime;
         var playerScreenPos = cam.WorldToScreenPoint(transform.position);//convert pos of player to screen pos
-        
+
+        target.x -= playerScreenPos.x;
+        target.y -= playerScreenPos.y;
+
+        float angle = Mathf.Atan2(target.y, target.x) * Mathf.Rad2Deg; // radians to degrees
+
+        transform.rotation = Quaternion.Euler(0, 0, angle);//degrees to quaternion
+
         //TODO: Target and Rotation
     }
 
