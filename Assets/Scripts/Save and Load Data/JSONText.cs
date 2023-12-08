3e3e7cb65df51fe56a4469697be6dfce50be35c4
XINLU CHEN <xinluchen.work@gmail.com>
2023-12-08 03:03:40 -0800

    small update in save and load data

--- Assets/Scripts/Save and Load Data/JSONText.cs
+++ Assets/Scripts/Save and Load Data/JSONText.cs
@@ -13,7 +13,7 @@ public class JSONText : MonoBehaviour
         sample.score = 20;
 
         string data = JsonUtility.ToJson(sample);
-        Debug.Log(data);
+        Debug.Log("ToJson: "+ data);
 
         // Deserializing JSON data
         string json = "{\n\t\"name\": \"Rico\", \n\t\"score\":  20\n}";
@@ -21,7 +21,7 @@ public class JSONText : MonoBehaviour
 
         SampleData sample2 = JsonUtility.FromJson<SampleData>(json);
 
-        Debug.Log("The sample data is " + sample2.name + "and " + sample2.score);
+        Debug.Log("The sample data is " + sample2.name + " and " + sample2.score);
     }
 
     // Update is called once per frame
